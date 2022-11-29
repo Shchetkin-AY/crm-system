aca575be5e7c97a401af577294229a1154b7dabd
Alexandr <shchetkin.a.y@gmail.com>
2022-11-30 00:12:39 +0700

    Added sructure and setup routing

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+      <router-view/>
+    </component>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
+
+export  default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta)
+      return 'empty-layout'
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~materialize-css/dist/css/materialize.min.css';
+  @import "assets/index.css";
+</style >
---
+++ src/router/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {layout: 'empty'},
+      component: () => import('../views/Login')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: {layout: 'main'},
+      component: () => import('../views/Categories')
+    }
+  ]
+})
+
+
